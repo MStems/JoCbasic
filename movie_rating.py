d87d099e29b7c24a62da612dfe27cc684e73ffb4
MStems <149131324+MStems@users.noreply.github.com>
2023-11-08 00:23:24 -0600

    Create movie_rating.py
    Program asking user to input rating and calculate diff between ratings

---
+++ movie_rating.py
@@ -0,0 +1,7 @@
+# Merrilee S
+# movie rating from user
+
+rating = eval(input("What is your rating of The Barbie Movie? "))
+mine = 3
+print("My rating is {mine}.")
+print("The difference between our ratings is:", (mine - rating))
