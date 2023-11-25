3006cf64c17a75a0b55b6c3afdc6d186f4b9ac07
MStems <149131324+MStems@users.noreply.github.com>
2023-11-25 03:05:30 -0600

    Create poem.py
    Request user inputs noun and adj. and insert into poem

---
+++ poem.py
@@ -0,0 +1,6 @@
+#  Create a program asking user for noun and adj to insert in poem
+
+Noun = input("Please give me a plural noun : ")
+A = input("Please give me an adjective: ")
+print(Noun, "are red, violets are blue")
+print("Monty Python is", A, "woo hoo!")
