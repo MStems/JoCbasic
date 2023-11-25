2f9547ad8cccc232b514ab02ff59f35252cf8b13
MStems <149131324+MStems@users.noreply.github.com>
2023-11-25 16:18:53 -0600

    Added round and sqrt functions

--- madfun.py
+++ madfun.py
@@ -2,7 +2,9 @@
 # Create program asking user for decimal number output absolute, 
 # rounded and sq.root of number
 
-
+import math
 dec = float(input("Please give me a decimal number: "))
 # print("The absolute value of", dec ,("= ") (abs(dec)))
 print("The absolute value of", dec ,"= ", abs(dec))
+print("The rounded value of", dec ,"= ", round(dec))
+print("The square root of", dec,"= ", math.sqrt(dec))
