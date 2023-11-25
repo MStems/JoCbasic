4b0511919f294559de56edde617d307cc5782310
MStems <149131324+MStems@users.noreply.github.com>
2023-11-25 14:54:49 -0600

    Create madfun.py
    Create program asking user for num input
    output absolute, rounded and sq root of num

---
+++ madfun.py
@@ -0,0 +1,8 @@
+# Merrilee S
+# Create program asking user for decimal number output absolute, 
+# rounded and sq.root of number
+
+
+dec = float(input("Please give me a decimal number: "))
+# print("The absolute value of", dec ,("= ") (abs(dec)))
+print("The absolute value of", dec ,"= ", abs(dec))
