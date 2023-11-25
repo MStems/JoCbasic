341132da2fa7caaaf0dff45621dab9c5badd806f
MStems <149131324+MStems@users.noreply.github.com>
2023-11-25 16:39:51 -0600

    Changed to rounded sqrt of abs value and changed spacing

--- madfun.py
+++ madfun.py
@@ -4,7 +4,7 @@
 
 import math
 dec = float(input("Please give me a decimal number: "))
-# print("The absolute value of", dec ,("= ") (abs(dec)))
+# print("The absolute value of", dec ,("=") (abs(dec)))
 print("The absolute value of", dec ,"= ", abs(dec))
-print("The rounded value of", dec ,"= ", round(dec))
-print("The square root of", dec,"= ", math.sqrt(dec))
+print("The rounded value of", dec ,"=", round(dec, 0))
+print("The rounded square root of the absolute value of", dec,"=", math.sqrt(abs(round(dec, 0))))
