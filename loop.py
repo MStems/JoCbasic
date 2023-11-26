cdce09071d741ccfc2d7607ad04edc0efbd5d3d7
MStems <149131324+MStems@users.noreply.github.com>
2023-11-25 18:31:42 -0600

    writing basic loops

---
+++ loop.py
@@ -0,0 +1,20 @@
+# Writing basic loop programs
+# Merrilee S
+
+for i in range(6):
+    print(i)
+
+for a in range(2,12,3):
+   print(a)
+
+for b in range(-10,1,2):
+    print(b)
+
+for m in range(4):
+    print('****')
+
+for char in ("CSCI 150"):
+    print(char)
+
+for c in range(0,11):
+   print(c)
